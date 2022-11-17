0ae82d00ce19f8dac9e8947251fa66be3cb06297
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2022-11-16 21:38:22 -0600

    first repository commit

---
+++ Tickets/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tickets
+{
+    public class Class1
+    {
+        ticketsEntities db = new ticketsEntities();
+
+        public List<Tickets.tickets> Listar()
+        {
+            var tickets = from a in db.tickets
+                          select a;
+            return tickets.ToList();
+        }
+
+        public Tickets.tickets Buscar(int id)
+        {
+            var tickets = from a in db.tickets
+                          where a.id == id
+                          select a;
+            return tickets.FirstOrDefault();
+        }
+
+        public void Modificar(tickets item)
+        {
+            var ticket = (from a in db.tickets
+                           where a.id == item.id
+                           select a).FirstOrDefault();
+            ticket.nombre = item.nombre;
+            ticket.descripcion = item.descripcion;
+            ticket.departamento = item.departamento;
+            db.SaveChanges();
+        }
+
+        public void Agregar(tickets item)
+        {
+            Console.WriteLine(item);
+            db.tickets.Add(item);
+            db.SaveChanges();
+        }
+
+        public void Delete(tickets item)
+        {
+            var ticket = (from a in db.tickets
+                           where a.id == item.id
+                           select a).FirstOrDefault();
+            db.tickets.Remove(ticket);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        Tickets.Class1 a = new Tickets.Class1();
+        // GET: Default
+        public ActionResult Index()
+        {
+            var tickets = a.Listar();
+            return View(tickets);
+        }
+        public ActionResult Index2()
+        {
+            var tickets = a.Listar();
+            return View(tickets);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(a.Buscar(id));
+        }
+        [HttpPost]
+
+        public ActionResult Edit(Tickets.tickets item)
+        {
+            a.Modificar(item);
+            return View("index",a.Listar());
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Tickets.tickets());
+        }
+        [HttpPost]
+
+        public ActionResult Create(Tickets.tickets item)
+        {
+            a.Agregar(item);
+            return View("index", a.Listar());
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(a.Buscar(id));
+        }
+        [HttpPost]
+        public ActionResult Delete(Tickets.tickets item)
+        {
+            a.Delete(item);
+            return View("index", a.Listar());
+        }
+    }
+}
